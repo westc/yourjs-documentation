7353394fd8dba2df5273fae05389e26601fc3229
Chris West <cwest-engineer@outlook.com>
2018-07-10 01:45:10 -0400

    Added max, maxBy, maxIndex, maxIndexBy, min, minBy, minIndex, minIndexBy

--- future functions.js
+++ future functions.js
@@ -3,3 +3,14 @@ var countBy = Function('G,H,T', ('return[@"number"!==typeof c[f=g?d(b[a],a,b):G(
     groupBy = countBy[1],
     indexBy = countBy[2];
 countBy = countBy[0];
+
+// requires getAt
+eval(
+  "in<ax>".replace(
+    /(..)(.)/g,
+    "!!b[d]!b[a]!!!!By!,e!a&&g?e(b[d]):G(b[d],e)!g?e(b[a]):G(b[a],e)!,i!e='string'===typeof e?[e]:e;!g='function'===typeof e,!".replace(
+      /([^! ]*)!([^!]*)!([^!]*)!([^!]*)!([^!]*)!([^!]*)!([^!]*)!/g,
+      "m$$1Index$1=function(b$2){$6for(var c,$7a=1,d=0,h=b.length,f=$3;a<h;a++)c=$4,c$$2f&&(f=c,d=a);return d},m$$1$1=function(a$5){return a[m$$1Index$1(a$5)]},"
+    )
+  ).replace(/(.+)./, 'var min,minBy,max,maxBy,minIndex,minIndexBy,maxIndex,maxIndexBy;min=function(G){$1}')
+)(getAt);
