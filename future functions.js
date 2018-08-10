6de5f1bc0488933f96652497f53cb046fa5b3d56
Chris West <cwest-engineer@outlook.com>
2018-08-10 06:45:37 -0400

    Update future functions.js

--- future functions.js
+++ future functions.js
@@ -1,1 +1,22 @@
 function mapFilter() {} // https://www.yourjs.com/console/?gist=f7062146ad31a501ec0e37f0f89c2eb0&file=mapFilter.js
+
+// Fix for time():  Added return for wrapper function
+function time(fn, times) {
+  var timesIsFunction = 'function' === typeof times;
+  return function() {
+    var startTime = +new Date,
+        result = fn.apply(this, arguments),
+        endTime = +new Date,
+        timingData = {
+          elapsed: endTime - startTime,
+          start: startTime,
+          end: endTime,
+          returnValue: result,
+          'function': fn,
+          'this': this,
+          'arguments': arguments
+        };
+    timesIsFunction ? times(timingData) : times.push(timingData);
+    return result;
+  };
+}
