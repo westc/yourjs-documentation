d5e4ac50d3f9d0ea5064caa2262485ad50564328
Chris West <cwest-engineer@outlook.com>
2018-08-24 12:46:26 -0400

    Update future functions.js

--- future functions.js
+++ future functions.js
@@ -4,23 +4,15 @@ function only() {} // https://www.yourjs.com/console/?gist=21095b56aa3aa03300fce
 
 function chunk() {} // https://www.yourjs.com/console/?gist=83212999f5de5dd92a6fa79bc4c6f285&file=chunk.js
 
-// Fix for time():  Added return for wrapper function
-function time(fn, times) {
-  var timesIsFunction = 'function' === typeof times;
-  return function() {
-    var startTime = +new Date,
-        result = fn.apply(this, arguments),
-        endTime = +new Date,
-        timingData = {
-          elapsed: endTime - startTime,
-          start: startTime,
-          end: endTime,
-          returnValue: result,
-          'function': fn,
-          'this': this,
-          'arguments': arguments
-        };
-    timesIsFunction ? times(timingData) : times.push(timingData);
-    return result;
-  };
+// Fix is() by removing explicit reference to YourJS
+function is(x, y) {
+  x = x === y
+    ? x !== 0 || 1 / x == 1 / y // -0 vs 0
+    : (x != x && y != y); // NaN
+  return x && arguments.length > 2
+    ? is.apply(0, slice(arguments, 1))
+    : x;
 }
+
+function includes() {} // https://www.yourjs.com/console/?gist=e7eb0bf516827fa7b0380d8da4fe05c6&file=includes+and+indexOf.js
+function indexOf() {} // https://www.yourjs.com/console/?gist=e7eb0bf516827fa7b0380d8da4fe05c6&file=includes+and+indexOf.js
