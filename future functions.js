86974c3e4ac4eeca4090146344f14f714783446c
Chris West <cwest-engineer@outlook.com>
2018-08-31 14:49:35 -0400

    Update future functions.js

--- future functions.js
+++ future functions.js
@@ -8,6 +8,14 @@ function includes() {} // https://www.yourjs.com/console/?gist=e7eb0bf516827fa7b
 
 function indexOf() {} // https://www.yourjs.com/console/?gist=e7eb0bf516827fa7b0380d8da4fe05c6&file=includes+and+indexOf.js
 
+function repeat() {} // https://www.yourjs.com/console/?gist=bb937db3001f6c1a91a68db34db7bf09&file=repeat.js
+
+function endsWith() {} // https://www.yourjs.com/console/?gist=eab12ae30a1991df0514350b6ad18552&file=endsWith.js
+
+function startsWith() {} // https://www.yourjs.com/console/?gist=7b98abf484dd8a3c10e4a33ffeee467e&file=startsWith.js
+
+function flatMap() {} // https://www.yourjs.com/console/?gist=c15e64baea1773744923777130feec68&file=flatMap.js
+
 // Fix is() by removing explicit reference to YourJS
 function is(x, y) {
   x = x === y
