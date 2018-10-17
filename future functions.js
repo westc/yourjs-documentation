ffa4bb0af1e56e96e9d39f8ff8d70c13dd443417
Chris West <cwest-engineer@outlook.com>
2018-10-17 01:09:07 -0400

    Update future functions.js

--- future functions.js
+++ future functions.js
@@ -1,1 +1,4 @@
-
+// A number to a range which will loop if the value passed is below or above the limits.
+function pacman(x, inclusiveLowerLimit, exclusiveUpperLimit) {
+  return YourJS.mod(x - inclusiveLowerLimit, exclusiveUpperLimit - inclusiveLowerLimit) + inclusiveLowerLimit;
+}
