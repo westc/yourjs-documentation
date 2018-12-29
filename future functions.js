b8c23ed39d136b45b6f9618068f7d6991c42e75a
Chris West <cwest-engineer@outlook.com>
2018-12-29 00:20:52 -0500

    Update future functions.js

--- future functions.js
+++ future functions.js
@@ -2,3 +2,7 @@ function pacman(x, inclusive, exclusive) {
   exclusive -= inclusive;
   return ((x - inclusive) % exclusive + exclusive) % exclusive + inclusive;
 }
+
+// only should work for something like the following:
+//   JS.only(o, (k, v) => v != undefined)
+// The above would essentially create a new object with all of the key-value pairs where the value is not null or undefined
