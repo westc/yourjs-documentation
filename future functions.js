524199f8bd01da2d9bfdcc02da6184c48ef9563b
Chris West <cwest-engineer@outlook.com>
2019-02-23 00:43:26 -0400

    Update future functions.js
    Added modDate()

--- future functions.js
+++ future functions.js
@@ -2,35 +2,19 @@
 //   JS.only(o, (k, v) => v != undefined)
 // The above would essentially create a new object with all of the key-value pairs where the value is not null or undefined
 
-// Scales a number using minX to maxX as the x range and minOut and maxReturn
-// as the corresponding return values range.
-function scale(x, minX, maxX, minReturn, maxReturn) {
-  return (x - minX) * (maxReturn - minReturn) / (maxX - minX) + minReturn;
-}
-
-// Specifies whether or not the number is negative (meaning it has a bit
-// indicating the sign of the number).
-function signbit(x) {
-  return x < 0 || 1 / x < 0;
-}
-
-// Replaces all of the instances of a target substring in subject with a
-// replacement string.  Gives the option to limit how many replacements occur.
-function replaceAll(subject, target, replacement, opt_limit) {
-  // reusing opt_limit to allow code to be minified even more by avoiding
-  // declaring an additional variable.
-  opt_limit = subject.split(
-    target = target + '',
-    (opt_limit == undefined || opt_limit === Infinity) ? undefined : opt_limit >= 1 ? +opt_limit + 1 : 1
-  );
-  opt_limit[opt_limit.length - 1] += subject.slice(opt_limit.join(target).length);
-  return opt_limit.join(replacement);
-}
-
-function hypot() {
-  var val, ret = 0, args = arguments, i = args.length;
-  while (i--) {
-    ret += (val = +args[i]) * val;
-  }
-  return Math.sqrt(ret);
-}
+// Modifies a date object with either offsets or specific times.
+var modDate;
+(function(MAP) {
+  modDate = function(date, objOffsets) {
+    for (var setter, getter, isSetOnly, k, v, keys = Object.keys(objOffsets), i = keys.length; i--; ) {
+      v = objOffsets[k = keys[i]];
+      isSetOnly = /^\$/.test(k);
+      k = k.replace(/^\$|s$/g, '').replace(/^((milli)?second|minute|hour)$/, '$&s').replace(/./, function(a) { return a.toUpperCase(); });
+      k = MAP[k] || k;
+      if ('function' === typeof date[setter = 'set' + k] && 'function' === typeof date[getter = 'get' + k]) {
+        date[setter](!isSetOnly ? date[getter]() + v : v);
+      }
+    }
+    return date;
+  };
+})({ Ms: 'Milliseconds', Day: 'Date', Year: 'FullYear' });
