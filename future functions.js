287b44a2d67b5b7285f3a508349138e7d104b8f1
Chris West <cwest-engineer@outlook.com>
2019-03-14 15:16:16 -0400

    Update future functions.js
    Update for throttle() and debounce().

--- future functions.js
+++ future functions.js
@@ -35,5 +35,27 @@ function replaceMany(subject, target, replacement, opt_limit) {
 
 
 
-// Updates matchAll() and execAll() so they can handle regular expressions like /(?:)/
+// Updates for matchAll() and execAll() so they can handle regular expressions like /(?:)/
 eval('exec:a,fmatch:f,a'.replace(/(\w+):(.,.)/g, 'function $1All($2,d){a=new RegExp(a.source,(a.flags||(a+"").replace(/[^]+\\//,"")).replace("g","")+"g");for(var b,g=0,c=[],e=0;b=a.exec(f);e===a.lastIndex&&++a.lastIndex,e=a.lastIndex)d&&(b=d(b,++g)),void 0!==b&&c.push(b);return c.length?c:null}'));
+
+
+
+
+// Updates for debounce() and throttle()
+// For both functions the first parameter is the function and the second is the amount of time to wait.
+// For debounce() the 3rd parameter (opt_initialAndAfter):
+// - True indicates that the function should run immediately and again after the allotted time has
+//   passed if the function is called multiple times.
+// - False indicates that the function should only be run on the intial call if the allotted time
+//   has passed since the previous call.
+// - If nothing is supplied or null or undefined then the function will only be called after the
+//   allotted amount of time has passed.
+// For throttle() the 3rd parameter (opt_leading):
+// - True indicates that the function should run immediately.  If called multiple times before the
+//   time has elapsed it will not be called again.
+// - False indicates that the function should only be run after the time has elapsed (not the initial
+//   call).
+// - If nothing is supplied or null or undefined then the function will be called on the initial and
+//   if called multiple times before the time elapses the final call will occur after the specified
+//   time has passed from the previous execution.
+"debounce231k=b;b=@;##b-k##@-b#throttle312#k=@,#k-b#b=k,#k-b#".replace(/(\D+)(.)(.)(.)([^#]*)#([^#]*)#([^#]*)#([^#]*)#([^#]*)#/g,"function $1(h,c,a){var d,e,f,g,k,b=0;a=void 0==a?$2:a?$3:$4;return function(){d=0;e=arguments;f=this;$5a&1&&($6d=$7>=c)&&($8g=h.apply(f,e));!d&&a&2&&setTimeout(function(){k=@,k-b>=c&&(b=k,g=h.apply(f,e))},c);return g}}").replace(/@/g,"+new Date")
