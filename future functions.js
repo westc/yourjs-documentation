11944f2d5ff905de22c7f3ef34b80cd2950f7af9
Chris West <cwest-engineer@outlook.com>
2019-03-19 10:42:52 -0400

    Update future functions.js
    Added flattenKeys()

--- future functions.js
+++ future functions.js
@@ -8,4 +8,28 @@ function ensureAt(root, path, opt_value) {
   return root;
 }
 
+function flattenKeys(obj, opt_keyedObjects) {
+  function recurse(base, path, keys, keysCount, ancestors) {
+    for (var innerKeys, innerKeysCount, v, k, i = 0; i < keysCount; i++) {
+      v = base[k = keys[i]];
+      innerKeys = v ? getKeys(v) : [];
+      if (ancestors.indexOf(v) < 0) {
+        k = k.replace(/\./g, '\\.');
+        if (innerKeysCount = innerKeys.length) {
+          recurse(v, path + k + '.', innerKeys, innerKeysCount, ancestors.concat([v]));
+        }
+        if (opt_keyedObjects || !innerKeysCount) {
+          result[path + k] = v;
+        }
+      }
+    }
+  }
+
+  var getKeys = Object.keys, result = {}, keys = obj ? getKeys(obj) : [], keysCount = keys.length;
+  if (keysCount) {
+    recurse(obj, '', keys, keysCount, [obj]);
+  }
+  return result;
+}
+
 // Update the documentation for set(...) to include the 4th parameter (opt_returnObj)
