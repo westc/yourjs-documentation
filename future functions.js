9ea63c5c0f57dcf9665e21cb07cd7179324ff85d
Chris West <cwest-engineer@outlook.com>
2019-03-19 10:53:56 -0400

    Update future functions.js

--- future functions.js
+++ future functions.js
@@ -8,13 +8,17 @@ function ensureAt(root, path, opt_value) {
   return root;
 }
 
+// Creates a new object with keys to all of the values regardless of the level
+// (excluding recursive objects).  Keys for values on deeper levels will be
+// joined by a "." while any dots or backslashes in the original keys will be
+// escaped with a leading blackslash.
 function flattenKeys(obj, opt_keyedObjects) {
   function recurse(base, path, keys, keysCount, ancestors) {
     for (var innerKeys, innerKeysCount, v, k, i = 0; i < keysCount; i++) {
       v = base[k = keys[i]];
       innerKeys = v ? getKeys(v) : [];
       if (ancestors.indexOf(v) < 0) {
-        k = k.replace(/\./g, '\\.');
+        k = k.replace(/[\\.]/g, '\\$&');
         if (innerKeysCount = innerKeys.length) {
           recurse(v, path + k + '.', innerKeys, innerKeysCount, ancestors.concat([v]));
         }
