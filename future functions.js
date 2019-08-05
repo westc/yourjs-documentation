5251eb0d02e7bc645bea4d38570ea3267ee382ed
Chris West <cwest-engineer@outlook.com>
2019-08-05 11:18:01 -0400

    Added a modification for modDate()

--- future functions.js
+++ future functions.js
@@ -40,3 +40,32 @@ function rekey(obj, keyMap, opt_keepOriginals) {
   }
   return newObj;
 }
+
+
+// Update this to allow for setting of DOW, min, and sec
+var modDate;
+(function (MAP) {
+  modDate = function (date, objOffsets) {
+    for (var setter, getter, isSetOnly, k, v, keys = Object.keys(objOffsets), i = keys.length; i--;) {
+      v = objOffsets[k = keys[i]];
+      isSetOnly = /^\$/.test(k);
+      k.replace(/^\$|\b(ms)$|s$/g, '$1')
+        .replace(/^((milli)?sec(ond)?|min(ute)?|hour)$/, '$&s')
+        .replace(/./, function (a) { return a.toUpperCase(); })
+        .replace(/^(DOW|dow)([0-6])?$|^.+$/i, function (k, dow, offset) {
+          if (dow) {
+            dow = (date.getDay() + -(offset || 0) + 7) % 7;
+            v = ((v % 7) + 7) % 7;
+            date.setDate(date.getDate() + (isSetOnly ? v - dow : ((dow + v) % 7 - dow)));
+          }
+          else {
+            k = MAP[k] || k;
+            if ('function' === typeof date[setter = 'set' + k] && 'function' === typeof date[getter = 'get' + k]) {
+              date[setter](!isSetOnly ? date[getter]() + v : v);
+            }
+          }
+        });
+    }
+    return date;
+  };
+})({ Ms: 'Milliseconds', Day: 'Date', Year: 'FullYear', Secs: 'Seconds', Mins: 'Minutes' });
