1dc40f435b47b1e3f88576cc90ac177a6c038360
Chris West <cwest-engineer@outlook.com>
2019-08-11 15:18:04 -0400

    Update future functions.js

--- future functions.js
+++ future functions.js
@@ -1,28 +1,46 @@
-function isInDateRange(date1, measure, date2) {
+function isInDateRange(measure, date1, date2) {
+  var args = arguments;
+  if (args.length < 3) {
+    return function() {
+      return isInDateRange.apply(this, slice(args).concat(slice(arguments)));
+    };
+  }
+  
+  var extremes = getDateExtremes(measure, date1);
+  return extremes[0] <= date2 && date2 <= extremes[1];
+}
+
+function getDateExtremes(measure, date) {
+  if (!date) {
+    return function(date) {
+      return getDateExtremes(measure, date);
+    };
+  }
+
   measure = measure.replace(/^DATE$/i, 'DAY');
   var measures = ['YEAR', 'MONTH', 'DAY', 'HOUR', 'MINUTE', 'SECOND'];
-  var measureFuncNames = ['FullYear', 'Month', 'Date', 'Hours', 'Minutes', 'Seconds'];
+  var measureFuncNames = ['Month', 'Date', 'Hours', 'Minutes', 'Seconds', 'Milliseconds'];
+  var maxes = [11, 31, 23, 59, 59, 999];
   var matchWeek = /^WEEK([0-6]?)$/i.exec(measure);
-  var measureIndex = matchWeek ? 1 : measures.indexOf(measure.toUpperCase());
+  var measureIndex = matchWeek ? 2 : measures.indexOf(measure.toUpperCase());
   if (measureIndex < 0) {
     throw new Error('Unknown date measure:\t' + measure);
   }
   
-  for (var fnName; measureIndex + 1; measureIndex--) {
-    fnName = 'get' + measureFuncNames[measureIndex];
-    if (date1[fnName]() !== date2[fnName]()) {
-      return false;
+  for (var fnName, date1 = new Date(date), date2 = new Date(date); measureIndex < 6;) {
+    fnName = 'set' + measureFuncNames[measureIndex];
+    date1[fnName](measureIndex === 1 ? 1 : 0);
+    date2[fnName](maxes[measureIndex++]);
+    if (measureIndex === 1 && date2.getDate() < 9) {
+      date2.setDate(0);
     }
   }
   if (matchWeek) {
     matchWeek = +(matchWeek[1] || 0);
-    date1 = new Date(date1);
     date1.setDate(date1.getDate() - (((date1.getDay() - matchWeek) % 7 + 7) % 7));
-    date2 = new Date(date2);
-    date2.setDate(date2.getDate() - (((date2.getDay() - matchWeek) % 7 + 7) % 7));
-    return date1.getDate() === date2.getDate();
+    date2.setDate(date2.getDate() - (((date2.getDay() - matchWeek) % 7 + 7) % 7) + 6);
   }
-  return true;
+  return [date1, date2];
 }
 
 function dow(date, opt_offset) {
