145ccc579cce025699bebdcffe4afd18aa8a1b4c
Chris West <cwest-engineer@outlook.com>
2019-09-07 21:04:01 -0400

    Update future functions.js

--- future functions.js
+++ future functions.js
@@ -25,3 +25,22 @@ eval('0call2[].slice.call(arguments,1)3arguments)}}0apply2b3b)}}'.replace(/\d/g,
     ')}:function(b){return a.apply(c,'
   ][i];
 }));
+
+// Allows for an array or an array like structure to be stepped through in the order specified
+// after each iteration.
+function step(array, callback, opt_stepValue, opt_startIndex) {
+  opt_stepValue = ~~opt_stepValue || 1;
+  var count = array.length;
+  var i = (opt_startIndex == undefined || isNaN(~~opt_startIndex))
+    ? opt_stepValue < 0
+      ? count - 1
+      : 0
+    : opt_startIndex;
+  i = (~~i % count + count) % count;
+  for (var newStep, value, result = []; 0 <= i && i < array.length; i += opt_stepValue) {
+    newStep = callback(value = array[i], i, array);
+    result.push(value);
+    opt_stepValue = newStep != undefined ? ~~newStep || 0 : opt_stepValue;
+  }
+  return result;
+}
