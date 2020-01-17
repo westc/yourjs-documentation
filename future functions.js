f7180d5ae1e0d536f72b84e7bfdaa5de5aedafea
Chris West <cwest-engineer@outlook.com>
2020-01-17 12:07:58 -0400

    Added unhtmlify()

--- future functions.js
+++ future functions.js
@@ -28,3 +28,13 @@ function pluck(array, path) {
 
 // YourJS.translate()
 // https://www.yourjs.com/console/?gist=63781eadb826a776a42c3221d6cb49cd&file=YourJS.translate.js
+
+// Takes an HTML string and turns it into a regular string by removing
+// tags and converting HTML entities into their corresponding characters.
+var unhtmlify;
+(function(div) {
+  unhtmlify = function(strHTML) {
+    div.innerHTML = strHTML;
+    return div.textContent || div.innerText;
+  }
+})(document.createElement('DIV'));
